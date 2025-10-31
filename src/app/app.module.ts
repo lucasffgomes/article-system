896dd0ed8724374038125277130a32fed76948ed
Lucas Gomes <lucasffgomes@hotmail.com>
2025-10-30 23:26:53 -0300

    feat: articles module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/users/users.module';
+import { ArticlesModule } from 'src/articles/articles.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ArticlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
