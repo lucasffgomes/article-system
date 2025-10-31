5f27b992fef172c52e44d99ecae4c8469e2a6405
Lucas Gomes <lucasffgomes@hotmail.com>
2025-10-30 23:28:48 -0300

    docs: added comment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+/**
+ * `src/app.module.ts`: Módulo principal do aplicativo.
+ * `src/app.controller.ts`: Define rotas e lida com requisições.
+ * `src/app.service.ts`: Contém lógica de negócio, separado do controller.
+ */
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
