100912e1e3597f91482446278a08e1d092e9d9fa
Lucas Gomes <lucasffgomes@hotmail.com>
2025-11-01 23:23:59 -0300

    chore: added first migration

---
+++ prisma/migrations/20251102022229_create_articles_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME
+);
